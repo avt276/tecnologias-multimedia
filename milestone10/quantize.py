f7df5c7a2522a7cb7b593dff7db88a480c5a7e94
Ana <avt276@inlumine.ual.es>
2020-12-03 20:08:11 +0100

    Adaptation.

--- milestone10/quantize.py
+++ milestone10/quantize.py
@@ -1,129 +1,142 @@
-## BR_Control
 #!/usr/bin/env python
 # PYTHON_ARGCOMPLETE_OK
 
-import argparse
-import sounddevice as sd
+''' Real-time Audio Intercommunicator (lossless compression of the chunks). '''
+
+import zlib
 import numpy as np
-import socket
-import time
-import psutil
-import math
 import struct
-import threading
+import math
 try:
     import argcomplete  # <tab> completion for argparse.
 except ImportError:
     print("Unable to import argcomplete")
 import minimal
-import buffer as bf
-import zlib as z
-import compress_order 
+import buffer
+import compress
 
-class BR_Control(compress_order.Compress):
-
+class BR_Control(compress.Compression):
     def __init__(self):
-        ''' Initializes the buffer. '''
-        super().__init__()
-        if minimal.args.buffering_time <= 0:
-            minimal.args.buffering_time = 1 # ms
-        print(f"buffering_time = {minimal.args.buffering_time} miliseconds")
         if __debug__:
-            print("chunks_to_buffer =", self.chunks_to_buffer)     
-    
-    def pack(self, chunk_number, chunk):
-        quantized_step = 700
-        quantized_chunk = deadzone_quantizer(chunk, quantized_step)
-        #print("---------Enviamos--------------")
-        #print(quantized_chunk)
-        packed_chunk = bf.Buffering.pack(chunk_number, quantized_chunk)
-        #print("---------Packed_Chunk--------------")
-        #print(packed_chunk)
-        return packed_chunk
+            print("Running BR_Control.__init__")
+        super().__init__()
+        if __debug__:
+            print("InterCom (BR_Control) is running")
 
-    def unpack(self, packed_chunk, dtype=minimal.Minimal.SAMPLE_TYPE):
-        (chunk_number, ) = bf.Buffering.unpack(packed_chunk)
-        quantized_chunk = packed_chunk[2:]
-        #print("---------Recibimos y desempaquetamos--------------")
-        #print(quantized_chunk)
-        quantized_step = 700
-        chunk = deadzone_dequantizer(quantize_chunk, quantized_step)
-        
-        return chunk_number, chunk
-              
-    def deadzone_quantizer(x, quantization_step):
+    def deadzone_quantizer(self, x, quantization_step):
         k = (x / quantization_step).astype(np.int)
         return k
 
-    def deadzone_dequantizer(k, quantization_step):
+    def deadzone_dequantizer(self, k, quantization_step):
         y = quantization_step * k
         return y  
-    
-    def run(self):
-        print("Press CTRL+c to quit")
-        self.played_chunk_number = 0
-        with self.stream(self._record_send_and_play):
 
-            first_received_chunk_number = self.receive_and_buffer()
-            if __debug__:
-                print("first_received_chunk_number =", first_received_chunk_number)
+    def pack(self, chunk_number, chunk):
+        quantized_step = 1
+        quantized_chunk = self.deadzone_quantizer(chunk, quantized_step)
+        quantized_chunk = super().pack(chunk_number, chunk)
+        return quantized_chunk
 
-            self.played_chunk_number = (first_received_chunk_number - self.chunks_to_buffer) % self.cells_in_buffer
-            # The previous selects the first chunk to be played the
-            # one (probably emptty) that are in the buffer
-            # self.chunks_to_buffer position before
-            # first_received_chunk_number.
+    def unpack(self, packed_chunk, dtype=minimal.Minimal.SAMPLE_TYPE):
+        (chunk_number, chunk) = super().unpack(packed_chunk, dtype)
+        quantized_step = 1
+        chunk = self.deadzone_dequantizer(chunk, quantized_step)
+        return chunk_number, chunk
 
-            while True:
-                self.receive_and_buffer()
-
-class BR_Control_verbose(BR_Control, compress_order.Compress__verbose):
+class BR_Control__verbose(BR_Control, compress.Compression__verbose):
     def __init__(self):
+        if __debug__:
+            print("Running BR_Control__verbose.__init__")
         super().__init__()
-        thread = threading.Thread(target=self.feedback)
-        thread.daemon = True # To obey CTRL+C interruption.
-        thread.start()
+        self.variance = np.zeros(self.NUMBER_OF_CHANNELS) # Variance of the chunks_per_cycle chunks.
+        self.entropy = np.zeros(self.NUMBER_OF_CHANNELS) # Entropy of the chunks_per_cycle chunks.
+        self.bps = np.zeros(self.NUMBER_OF_CHANNELS) # Bits Per Symbol of the chunks_per_cycle compressed chunks.
+        self.chunks_in_the_cycle = []
 
-    def feedback(self):
-        while True:
-            time.sleep(self.SECONDS_PER_CYCLE)
-            self.cycle_feedback()
+        self.average_variance = np.zeros(self.NUMBER_OF_CHANNELS)
+        self.average_entropy = np.zeros(self.NUMBER_OF_CHANNELS)
+        self.average_bps = np.zeros(self.NUMBER_OF_CHANNELS)
 
-    def send(self, packed_chunk):
-        BR_Control.send(self, packed_chunk)
-        self.sent_bytes_count += len(packed_chunk)
-        self.sent_messages_count += 1
+    def stats(self):
+        string = super().stats()
+        string += " {}".format(['{:9.0f}'.format(i) for i in self.variance])
+        string += " {}".format(['{:4.1f}'.format(i) for i in self.entropy])
+        string += " {}".format(['{:4.1f}'.format(i/self.frames_per_cycle) for i in self.bps])
+        return string
 
-    def receive(self):
-        packed_chunk = super().receive()
-        self.received_bytes_count += len(packed_chunk)
-        self.received_messages_count += 1
-        return packed_chunk
+    def first_line(self):
+        string = super().first_line()
+        string += "{:27s}".format('') # variance
+        string += "{:17s}".format('') # entropy
+        string += "{:17s}".format('') # bps
+        return string
 
-    def _record_send_and_play(self, indata, outdata, frames, time, status):
-        if minimal.args.show_samples:
-            self.show_indata(indata)
+    def second_line(self):
+        string = super().second_line()
+        string += "{:>27s}".format("variance") # variance
+        string += "{:>17s}".format("entropy") # entropy
+        string += "{:>17s}".format("BPS") # bps
+        return string
 
-        super()._record_send_and_play(indata, outdata, frames, time, status)
+    def separator(self):
+        string = super().separator()
+        string += f"{'='*(27+17*2)}"
+        return string
 
-        if minimal.args.show_samples:
-            self.show_outdata(outdata)
+    def averages(self):
+        string = super().averages()
+        string += " {}".format(['{:9.0f}'.format(i) for i in self.average_variance])
+        string += " {}".format(['{:4.1f}'.format(i) for i in self.average_entropy])
+        string += " {}".format(['{:4.1f}'.format(i/self.frames_per_cycle) for i in self.average_bps])
+        return string
+        
+    def entropy_in_bits_per_symbol(self, sequence_of_symbols):
+        value, counts = np.unique(sequence_of_symbols, return_counts = True)
+        probs = counts / len(sequence_of_symbols)
+        #n_classes = np.count_nonzero(probs)
 
-    def run(self):
-        '''.'''
-        print("Press CTRL+c to quit")
-        self.print_header()
+        #if n_classes <= 1:
+        #    return 0
+
+        entropy = 0.
+        for i in probs:
+            entropy -= i * math.log(i, 2)
+
+        return entropy
+
+    def cycle_feedback(self):
         try:
-            self.played_chunk_number = 0
-            with self.stream(self._record_send_and_play):
-                first_received_chunk_number = self.receive_and_buffer()
-                if __debug__:
-                    print("first_received_chunk_number =", first_received_chunk_number)
-                self.played_chunk_number = (first_received_chunk_number - self.chunks_to_buffer) % self.cells_in_buffer
-                while True:
-                    self.receive_and_buffer()
-        except KeyboardInterrupt:
-            self.print_final_averages()
+            concatenated_chunks = np.vstack(self.chunks_in_the_cycle)
+        except ValueError:
+            concatenated_chunks = np.vstack([self.zero_chunk, self.zero_chunk])
+        
+        self.variance = np.var(concatenated_chunks, axis=0)
+        self.average_variance = self.moving_average(self.average_variance, self.variance, self.cycle)
+
+        self.entropy[0] = self.entropy_in_bits_per_symbol(concatenated_chunks[:, 0])
+        self.entropy[1] = self.entropy_in_bits_per_symbol(concatenated_chunks[:, 1])
+        self.average_entropy = self.moving_average(self.average_entropy, self.entropy, self.cycle)
+
+        self.average_bps = self.moving_average(self.average_bps, self.bps, self.cycle)
+
+        super().cycle_feedback()
+        self.chunks_in_the_cycle = []
+        self.bps = np.zeros(self.NUMBER_OF_CHANNELS)
+        
+    def _record_send_and_play(self, indata, outdata, frames, time, status):
+        super()._record_send_and_play(indata, outdata, frames, time, status)
+        self.chunks_in_the_cycle.append(indata)
+        # Remember: indata contains the recorded chunk and outdata,
+        # the played chunk.
+
+    def unpack(self, packed_chunk, dtype=minimal.Minimal.SAMPLE_TYPE):
+        (chunk_number, len_compressed_channel_0) = struct.unpack("!HH", packed_chunk[:4])
+        len_compressed_channel_1 = len(packed_chunk[len_compressed_channel_0+4:])
+
+        self.bps[0] += len_compressed_channel_0*8
+        self.bps[1] += len_compressed_channel_1*8
+        chunk_number, chunk = super().unpack(packed_chunk, dtype)
+        return chunk_number, chunk
 
 if __name__ == "__main__":
     minimal.parser.description = __doc__
@@ -136,9 +149,9 @@ if __name__ == "__main__":
             pass
     minimal.args = minimal.parser.parse_known_args()[0]
     if minimal.args.show_stats or minimal.args.show_samples:
-        intercom = BR_Control_verbose
+        intercom = BR_Control__verbose()
     else:
-        intercom = BR_Control()
+        intercom = BR_Control__verbose()
     try:
         intercom.run()
     except KeyboardInterrupt:
